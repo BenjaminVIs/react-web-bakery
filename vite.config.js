70b854cf24f5c0fea8eec9f29ffb12034a47dd59
BenjaminVIs <bg6678522@gmail.com>
2025-11-25 21:23:14 -0300

    fix: base path en vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-web-bakery/',
   plugins: [react()],
+  base: '/'  // ← IMPORTANTE: debe ser '/'
 })
